3af1e873363ba00c72f45da775e3fc4d406f5ca0
Daniel Bednarek <daniel.bednarek@globallogic.com>
2022-07-12 09:56:15 +0200

    smol improvements

--- public/scripts/buttonHandler.js
+++ public/scripts/buttonHandler.js
@@ -1,7 +1,17 @@
 socket = new WebSocket(`ws://${window.location.host}/messages`);
 
+const rightDiv = document.querySelector(".right");
+const terminal = document.querySelector(".terminal");
+
 const sleepBtn = document.getElementById("sleepBtn");
 
 sleepBtn.addEventListener("click", () => {
-    socket.send("sleep message (TEST)");
+    let message = "sleep message (TEST)"
+    socket.send(message);
+    const newMessageElement = document.createElement("p");
+    newMessageElement.textContent = message;
+    terminal.appendChild(newMessageElement);
+    if (isUserNearBottom(terminal)) {
+        terminal.scrollTop = newMessageElement.offsetTop;
+    }
 });
